642837179a063fb415c2eb56e2294e246aefae52
Ovuota Godspower <ovuotex1@gmail.com>
2020-05-19 23:53:58 +0100

    Hospital project
    School project

---
+++ Hospital.js
@@ -0,0 +1,65 @@
+function myFuction(){
+  var x = document.getElementById("myDiv");
+  if(x.style.display === "block"){
+    x.style.display == "none";
+  }else{
+    x.style.display == "none";
+  }
+}
+
+function myFun(){
+  var x = document.getElementById("myDate");
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  }else{
+    x.style.display = "none";
+  }
+}
+function myAppointment(){
+  var x = document.getElementById('myBook')
+  if(x.style.display === "none"){
+    x.style.display = "block";
+  }else{
+    x.style.display = "none";
+  }
+};
+
+function reg(){
+  var x = document.getElementById('myReg')
+  if(x.style.display === "none"){
+    x.style.display = "block";
+  }else{
+    x.style.display = "none";
+  }
+}
+function myLogin(){
+  var x = document.getElementById("hide-reg");
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }else{
+    x.style.display = "block";
+  }
+
+}
+
+function myLogin(){
+  var x = document.getElementById("hide-reg");
+  if(x.style.display == "none"){
+    x.style.display = "block";
+  }else{
+    x.style.display = "none";
+  }
+
+}
+function closeReg(){
+  var x = document.getElementById("myReg");
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }else{
+    x.style.display = "block";
+  }
+
+}
+
+
+
